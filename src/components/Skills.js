5e9500aaec5b2a7da7a00b34548049fbfd6fd69a
Abhishek279-alt <arabhishek999@gmail.com>
2023-10-17 09:23:58 +0530

    added experience

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,8 +2,11 @@ import React from "react";
 
 const Skills = () => {
   return (
-    <section id="myportfolio_skills" className="py-3 container-fluid">
-      <div className="row">
+    <section
+      id="myportfolio_skills"
+      className="py-3 container-fluid d-flex flex-lg-row flex-md-column flex-sm-column"
+    >
+      <div className="row" style={{ flexBasis: "60%" }}>
         <div className="col-12 text-secondary text-center fs-2 fw-bold text-decoration-underline my-5">
           Skills
         </div>
@@ -74,45 +77,21 @@ const Skills = () => {
                 />
               </div>
             </li>
-            <li className="py-2">
-              <span>Node.js</span>
-              <div className="progress" style={{ height: "2px" }}>
-                <div
-                  className="progress-bar bg-secondary"
-                  role="progressbar"
-                  style={{ width: "50%" }}
-                  aria-valuenow={25}
-                  aria-valuemin={0}
-                  aria-valuemax={100}
-                />
-              </div>
-            </li>
-            <li className="py-2">
-              <span>MySql</span>
-              <div className="progress" style={{ height: "2px" }}>
-                <div
-                  className="progress-bar bg-secondary"
-                  role="progressbar"
-                  style={{ width: "60%" }}
-                  aria-valuenow={25}
-                  aria-valuemin={0}
-                  aria-valuemax={100}
-                />
-              </div>
-            </li>
-            <li className="py-2">
-              <span>MongoDb</span>
-              <div className="progress" style={{ height: "2px" }}>
-                <div
-                  className="progress-bar bg-secondary"
-                  role="progressbar"
-                  style={{ width: "45%" }}
-                  aria-valuenow={25}
-                  aria-valuemin={0}
-                  aria-valuemax={100}
-                />
-              </div>
-            </li>
+          </ul>
+        </div>
+      </div>
+      <div
+        className="row"
+        style={{ flexBasis: "40%", backgroundColor: "#aca7cb" }}
+      >
+        <div className="col-12 text-white text-center fs-2 fw-bold text-decoration-underline my-5">
+          Experience
+        </div>
+        <div className="col-lg-5 col-md-6 col-sm-8 skills mx-auto">
+          <ul>
+            <li className="my-2 text-white">Company - HomzNOffiz Pvt. Ltd.</li>
+            <li className="my-2 text-white">Internship | 3 months</li>
+            <li className="my-2 text-white">Role - Frontend Developer</li>
           </ul>
         </div>
       </div>
